7639d4af4d7cd2652230357d437f25c0005085b1
Anndony Quemag <anndony.kommit@gmail.com>
2021-09-01 11:19:13 -0500

    chapter changes 4 Supervisor and Application, 5 Dynamic supervisors, 6 ETS

--- kv/test/kv_test.exs
+++ kv/test/kv_test.exs
@@ -2,8 +2,8 @@ defmodule KVTest do
   use ExUnit.Case
   doctest KV
 
-  test "greets the world" do
-    assert KV.hello() == :world
-    # assert KV.hello() == :oops
-  end
+  # test "greets the world" do
+  #   assert KV.hello() == :world
+  #   # assert KV.hello() == :oops
+  # end
 end
